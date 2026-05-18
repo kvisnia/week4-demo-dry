96730d8425a5d66d595d1359d3c61f3ca39fc584
KV <kvisnia@gmail.com>
2026-05-18 18:24:40 +0200

    test ci/cd

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Hello World! Week 4
+      <h1>Hello World! Week 4!
       </h1>
+      <h2>Adding some automation here.</h2>
     </div>
   )
 }
